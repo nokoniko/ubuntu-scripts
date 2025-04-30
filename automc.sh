7b252d13adcfe2b833b2b88c02afcc7ad801c034
Niko <94224292+nokoniko@users.noreply.github.com>
2025-04-30 12:37:51 +0200

    Update automc.sh

--- automc.sh
+++ automc.sh
@@ -12,10 +12,11 @@ sudo apt update
 sudo apt install tmux openjdk-21-jdk-headless -y
 
 # Last ned PaperMC (Java-server som støtter plugins)
-wget https://api.papermc.io/v2/projects/paper/versions/1.21.4/builds/227/downloads/paper-1.21.4-227.jar -O paper.jar
+wget https://api.papermc.io/v2/projects/paper/versions/1.21.4/builds/227/downloads/paper-1.21.4-227.jar
 
 # Lag plugins-mappen og last ned Geyser
 mkdir -p plugins 
 wget https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot -O plugins/Geyser-Spigot.jar
 
-echo "‘\033[0;32mfår å starte serveren gjør tmux new-session -d -s minecraft "java -Xmx1024M -Xms1024M -jar paper.jar nogui""
+# Start serveren i bakgrunnen med tmux
+echo "får å starte serveren gjør tmux new-session -d -s minecraft "java -Xmx1024M -Xms1024M -jar paper.jar nogui""
