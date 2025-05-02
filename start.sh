aef0fc784ba5bc26abbba6274230f57ea47ce913
Niko <94224292+nokoniko@users.noreply.github.com>
2025-05-02 14:36:44 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,13 +14,13 @@ read "er det en vanilla eller paper server vanilla/paper: " valg
 #                                                        ------ bedrock ------
 # om man svarer bedrock vill den starte paper serveren    
 if [ "$valg" = "bedrock"]                                  
-# starter paper servere med tmux                                        
-tmux new-session -s minecraft "java -Xmx1024M -Xms1024M -jar paper-1.21.4-227.jar nogui"
-# om man svarer vanilla vill den starte en vanilla/mojang server
-#                                                        ------ VANILLA ------
+    # starter paper servere med tmux                                        
+    tmux new-session -s minecraft "java -Xmx1024M -Xms1024M -jar paper-1.21.4-227.jar nogui"
+    # om man svarer vanilla vill den starte en vanilla/mojang server
+    #                                                        ------ VANILLA ------
 elif [ "$valg" = "vanilla"]
-# starter vanilla serveren med tmux
-tmux new-session -s minecraft "java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui"
+    # starter vanilla serveren med tmux
+    tmux new-session -s minecraft "java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui"
 else
     echo "Ugyldig valg. Skriv enten vanilla eller paper."
 fi
