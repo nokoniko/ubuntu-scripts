d1a888cb4c946fb3bb3d3c804f2d91a265f5ea03
Niko <94224292+nokoniko@users.noreply.github.com>
2025-05-02 17:26:01 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # spør om du vill ha bedrock eller ikke 
-read "er det en vanilla eller paper server vanilla/paper: " valg
-# starter en ny temux session
+read "\e[32mer det en vanilla eller paper server vanilla/paper: \e[0m" valg
 #                                                        ------ bedrock ------
 # om man svarer bedrock vill den starte paper serveren    
+# Tøm eventuell eksisterende tmux session
+tmux has-session -t minecraft 2>/dev/null
+    if [ $? == 0 ]; then
+        tmux kill-session -t minecraft
+        echo -e "\e[32mTømte eksisterende tmux-session.\e[0m"
+    fi
+
 if [ "$valg" = "bedrock"]                                  
     # starter paper servere med tmux                                        
     tmux new-session -s minecraft "java -Xmx1024M -Xms1024M -jar paper-1.21.4-227.jar nogui"
@@ -14,5 +20,5 @@ elif [ "$valg" = "vanilla"]
     # starter vanilla serveren med tmux
     tmux new-session -s minecraft "java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui"
 else
-    echo "Ugyldig valg. Skriv enten vanilla eller paper."
+    echo "\e[31mUgyldig valg. Skriv enten vanilla eller paper.\e[0m"
 fi
