8beb456ec3c8cbcfd0f5252956bea783a12746cd
Niko <94224292+nokoniko@users.noreply.github.com>
2025-05-02 16:57:52 +0200

    Create online.sh

---
+++ online.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Dette scriptet oppdaterer seg automatisk, så du ikke trenger å laste det ned hver gang
+bash <(curl -s https://raw.githubusercontent.com/nokoniko/automatisk-minecraft-server-ubuntu/main/serveropset.sh)
