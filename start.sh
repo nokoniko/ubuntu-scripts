494b96bc99a64cbae0713f68707cb0d30730d7c3
Niko <94224292+nokoniko@users.noreply.github.com>
2025-05-02 12:37:24 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,25 +6,21 @@ ls
 echo "hvilken mappe vill du gå in i? "
 read mappenavn
 
-cd "$mappenavn"
+cd "$mappenavn" || exit
 
 # spør om du vill ha bedrock eller ikke 
 read "er det en vanilla eller paper server vanilla/paper: " valg
 # starter en ny temux session
-
-#                                                    ------ bedrock ------
-
-# om man svarer bedrock vill den starte paper serveren
-if [ "$valg" = "bedrock"]
-
-# starter paper servere med tmux
+#                                                        ------ bedrock ------
+# om man svarer bedrock vill den starte paper serveren    
+if [ "$valg" = "bedrock"]                                  
+# starter paper servere med tmux                                        
 tmux new-session -s minecraft "java -Xmx1024M -Xms1024M -jar paper-1.21.4-227.jar nogui"
-
 # om man svarer vanilla vill den starte en vanilla/mojang server
-
-#                                                     ------ VANILLA ------
-
+#                                                        ------ VANILLA ------
 elif [ "$valg" = "vanilla"]
-
 # starter vanilla serveren med tmux
 tmux new-session -s minecraft "java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui"
+else
+    echo "Ugyldig valg. Skriv enten vanilla eller paper."
+fi
